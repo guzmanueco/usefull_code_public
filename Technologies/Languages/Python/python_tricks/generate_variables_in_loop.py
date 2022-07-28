5683b6fec81e0b838cca5bbab601a371ca853788
Guzman <guzman.garcia@vass.es>
2022-07-28 12:32:57 +0200

    Added link to generate_variables_in_loop.py

--- Technologies/Languages/Python/python_tricks/generate_variables_in_loop.py
+++ Technologies/Languages/Python/python_tricks/generate_variables_in_loop.py
@@ -1,4 +1,6 @@
 # create variables with a loop and assign to them different names
+# Useful link about globals() in python: https://www.programiz.com/python-programming/methods/built-in/globals
+
 for i in ['1', '2', '3', '4', '5']:
     globals()[f"var_{i}"] = i
 for var in [var_1, var_2, var_3, var_4, var_5]:
